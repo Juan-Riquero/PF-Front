37f749a2d107a6b4fd35f9d1b82ad951131e012e
Nicolás Cáceres Sala <ncsala@gmail.com>
2022-05-10 23:51:12 -0300

    Se crea componente SearchBar con sus respectivas actions y reducer.
    Se crea en archivos separados hay que adaptarlo al index

--- src/Components/SearchBar/index.jsx
+++ src/Components/SearchBar/index.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
+import { searchByName } from "../../Redux/Actions/searchAction";
 
 
 export default function SearchBar({ setCurrentPage }) {
   const dispatch = useDispatch();
 
+  const [searchValue, setSearchValue ] = useState("");
+
+  const handleChange = (e) => {
+    const inputValue = e.target.value;
+    e.preventDefault();
+    setSearchValue(inputValue);
+    // setCurrentPage(1);
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    dispatch(searchByName(searchValue));
+  }
+
   return (
-    <div >
-     
-    </div>
-  );
+    <>
+      <form onSubmit={handleOnSubmit}>
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={handleChange}
+          pattern="[a-zA-ZñÑáéíóúÁÉÍÓÚ\-., ]{3,100}"
+          title="Search must be between 3 and 100 characters"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </>
+  )
 }
+
