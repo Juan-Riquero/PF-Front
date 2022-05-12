5f39e19bfedde9479661419b8521e6ad96c73a0e
juan riquero <juanoriquero@cmail.comcd>
2022-05-12 19:56:31 -0300

    ana me dijo que haga esto, es su culpa

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 
 function Card({ sneaker }) {
-  const { name, color, details, price} = sneaker;
+  const { name, price, grid_picture_url,brand_name} = sneaker;
   
   return (
     <div>
       <h2>{name}</h2>
-      <h3>{color}</h3>
-      <h3>{sneaker.details}</h3>
-      <h3>{sneaker.price}</h3>
+      <img
+          src={grid_picture_url}
+          alt=""
+          className="imageCard"
+          height="200px"
+          width="350px"
+        />
+      <h3>{brand_name}</h3>
+      <h3>{price}</h3>
     </div>
   );
 }
--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,9 +1,9 @@
 import { Link, useParams } from "react-router-dom";
 import "./detail.css";
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+/* import { useDispatch, useSelector } from "react-redux";
 import { getDetail, deleteVideogame } from "../../Redux/Actions";
-import { useState } from "react";
+import { useState } from "react"; */
 
 export default function Detail() {
   const dispatch = useDispatch();
@@ -11,6 +11,27 @@ export default function Detail() {
 
   return (
     <div > 
+         <SearchBar />
+            <div>
+              <img src={grid_picture_url} alt={'img'} />
+              <div>
+                <h3 >{brand_name}</h3>
+                <h6>Price: {price}</h6>
+                <h6>Details: {details}</h6>
+                <h6>Sizes: {sizes}</h6>
+                <h6>Model: {name}</h6>  
+                <div>
+                  <Link  to="/sneakers">
+                    GO BACK
+                  </Link>
+                  <button>
+                    Add to Cart                
+                  </button>
+                </div>
+
+              </div>
+            </div>
+           
   
     </div>
   );
